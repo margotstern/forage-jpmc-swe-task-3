fbc9f5d501321f9523b9126bd46580ef00a396cf
margotstern <margot.stern02@gmail.com>
2023-08-15 13:06:04 -0400

    completed task 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,14 +1,22 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
+  
+  
+  static generateRow(serverRespond: ServerRespond[]) {
+    const priceABC = (serverRespond[0].top_ask_price + serverRespond[0].top_bid.price) /2;
+    
     return serverResponds.map((el: any) => {
       return {
         stock: el.stock,
